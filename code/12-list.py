50db4318f3f54466bcb7b0760abb7d8d3bc65bd3
Songsihyeon <s20040@gsm.hs.kr>
2021-02-13 21:51:59 +0900

    programmers with python - (12) list

--- code/12-list.py
+++ code/12-list.py
@@ -3,4 +3,5 @@
 def solution(array, commands): 
     return [sorted(array[x[0]-1:x[1]])[x[2]-1] for x in commands]
 
+# 테스트 케이스
 print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
